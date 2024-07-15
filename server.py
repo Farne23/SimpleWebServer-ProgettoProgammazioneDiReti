9ef6102c623d8e015472c577298dd095eb33a296
Michele farneti <michele.farneti@studio.unibo.it>
2024-07-15 15:14:45 +0200

    Added header to server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+'''
+    Matricola: 0001080677
+    Email: michele.farneti@studio.unibo.it
+    Anno: 2024
+
+    Traccia 2:
+    Creare un web server semplice in Python che possa servire file statici (come HTML, CSS, immagini)
+    e gestire richieste HTTP GET di base. Il server deve essere in grado di gestire più richieste
+    simultaneamente e restituire risposte appropriate ai client.
+'''
+
